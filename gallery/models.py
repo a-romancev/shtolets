cd05304c2d715d86167997db9362892a29011105
Pandalad1n <Xpandalad1nX@gmail.com>
2019-01-13 17:25:35 +0300

    gallery update

--- gallery/models.py
+++ gallery/models.py
@@ -5,7 +5,12 @@ class Picture(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
     image = models.ImageField(upload_to='gallery/')
+    preview = models.ImageField(upload_to='gallery/')
 
     @property
     def url(self):
         return '/' + self.image.url
+
+    @property
+    def preview_url(self):
+        return '/' + self.preview.url
