041d4ae932b6020eba2ee4bd3262141e19113a03
Pandalad1n <Xpandalad1nX@gmail.com>
2019-01-27 15:35:18 +0300

    Refactor gallery

--- static/js/gallery.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    let picture = $('.js-fullsize-img')
-
-    $('.js-preview').click(function (e) {
-        picture.attr('src', $(e.currentTarget).attr('data-fullsize-url'))
-    })
-})
