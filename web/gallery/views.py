b301906115de8abc5c938168a1d8807221a1d484
Pandalad1n <Xpandalad1nX@gmail.com>
2019-02-03 17:34:56 +0300

    changed pic order

--- web/gallery/views.py
+++ web/gallery/views.py
@@ -5,5 +5,5 @@ from gallery.models import Picture
 
 class Gallery(generic.View):
     def get(self, request):
-        pics = Picture.objects.all()
+        pics = Picture.objects.all().order_by('id')
         return render(request, 'home/gallery.html', {'pics': pics})
